5eb5d7b56885bbe3ebf3cf6969a36d0645cc3adc
Lennox Kabo <borelennox@gmail.com>
2024-12-28 21:36:53 +0300

    mobile responsieveness plus mobile header complete

--- partials/footer.php
+++ partials/footer.php
@@ -1,54 +1,62 @@
 <footer>
-        <div class="footer_top">
-            <div class="footer_left">
-                <div class="footer_logo">
-                    <img src="assets/images/weldmartlogo1.png" alt="Weldmart Logo">
-                </div>
-                <p class="footer_description">
-                    WeldMart Enterprise Kenya is a premier supplier specializing in high-quality welding rods, MIG wire, and galvanized perforated metal sheets. Catering to wholesale markets and industrial clients, we are committed to delivering top-notch products that meet the diverse needs of our customers in Kenya and the East African region.
-                </p>
-                <div class="footer_socials">
-                    <i class="fa-brands fa-whatsapp fa-xl"></i>
-                    <i class="fa-brands fa-instagram fa-xl"></i>
-                    <i class="fa-brands fa-facebook fa-xl"></i>
-                    <i class="fa-brands fa-twitter fa-xl"></i>
-                </div>
+    <div class="footer_top">
+        <div class="footer_left">
+            <div class="footer_logo">
+                <img src="assets/images/weldmartlogo1.png" alt="Weldmart Logo">
             </div>
-            <div class="footer_right">
-                <div class="footer_links">
-                    <h4>Quick Links</h4>
-                    <a href="index.php">Home</a>
-                    <a href="about.php">About Us</a>
-                    <a href="products.php">Products</a>
-                    <a href="contact.php">Contact Us</a>
-                </div>
-                <div class="footer_links">
-                    <h4>Products</h4>
-                    <a href="products.php">Welding Rods</a>
-                    <a href="products.php">MIG Wire</a>
-                    <a href="products.php">Galvanized Perforated Metal Sheets</a>
-                </div>
-                <div class="footer_links">
-                    <h4>Business Hours</h4>
-                    <p>Mon- Friday: 8am - 5pm</p>
-                    <p>Saturday: 8am - 2pm</p>
-                </div>
-                <div class="footer_links">
-                    <h4>Contact Us</h4>
-                    <p>Tel 1: +254 711 288688</p>
-                    <p>Email: info@weldmart.com</p>
-                    <p>Address: Gikomba</p>
-                </div>
+            <p class="footer_description">
+                WeldMart Enterprise Kenya is a premier supplier specializing in high-quality welding rods, MIG wire, and galvanized perforated metal sheets. Catering to wholesale markets and industrial clients, we are committed to delivering top-notch products that meet the diverse needs of our customers in Kenya and the East African region.
+            </p>
+            <div class="footer_socials">
+                <i class="fa-brands fa-whatsapp fa-xl"></i>
+                <i class="fa-brands fa-instagram fa-xl"></i>
+                <i class="fa-brands fa-facebook fa-xl"></i>
+                <i class="fa-brands fa-twitter fa-xl"></i>
             </div>
         </div>
-        <div class="footer_bottom">
-            <p class="copyright_text">
-                © Copyright 2024 - Weldmart Enterprise Kenya - All Rights Reserved
-            </p>
-            <p class="developer_text">
-                Developed by<span> Enchanted Tech</span>
-            </p>
+        <div class="footer_right">
+            <div class="footer_links">
+                <h4>Quick Links</h4>
+                <a href="index.php">Home</a>
+                <a href="about.php">About Us</a>
+                <a href="products.php">Products</a>
+                <a href="contact.php">Contact Us</a>
+            </div>
+            <div class="footer_links">
+                <h4>Products</h4>
+                <a href="products.php">Welding Rods</a>
+                <a href="products.php">MIG Wire</a>
+                <a href="products.php">Galvanized Perforated Metal Sheets</a>
+            </div>
+            <div class="footer_links">
+                <h4>Business Hours</h4>
+                <p>Mon- Friday: 8am - 5pm</p>
+                <p>Saturday: 8am - 2pm</p>
+            </div>
+            <div class="footer_links">
+                <h4>Contact Us</h4>
+                <p>Tel 1: +254 711 288688</p>
+                <p>Email: info@weldmart.com</p>
+                <p>Address: Gikomba</p>
+            </div>
         </div>
-    </footer>
+    </div>
+    <div class="footer_bottom">
+        <p class="copyright_text">
+            © Copyright 2024 - Weldmart Enterprise Kenya - All Rights Reserved
+        </p>
+        <p class="developer_text">
+            Developed by<span> Enchanted Tech</span>
+        </p>
+    </div>
+</footer>
 
-    <script src="assets/js/cart.js"></script>
+<script src="assets/js/cart.js"></script>
+<script>
+    // JavaScript for Hamburger Menu Toggle
+    document.getElementById('hamburger').addEventListener('click', function() {
+        const mobileNav = document.getElementById('mobile-nav');
+        this.classList.toggle('active');
+        mobileNav.style.display = mobileNav.style.display === 'block' ? 'none' : 'block';
+    });
+</script>
--- partials/header.php
+++ partials/header.php
@@ -1,7 +1,7 @@
 <?php
 $currentPage = basename($_SERVER["PHP_SELF"]);
 ?>
-<header>
+<header class="desktop-header">
     <div class="header_logo">
         <img src="assets/images/weldmartlogo1.png" alt="Weldmart Logo">
     </div>
@@ -24,7 +24,7 @@ $currentPage = basename($_SERVER["PHP_SELF"]);
             <div class="cart_icon">
                 <a href="cart.php"><i class="fa-solid fa-cart-shopping fa-lg"></i></a>
             </div>
-            <div id="cart-counter">1</div>
+            <div id="cart-counter">0</div>
         </div>
         <div>
             <div class="whatsapp_icon">
@@ -35,3 +35,36 @@ $currentPage = basename($_SERVER["PHP_SELF"]);
         </div>
     </div>
 </header>
+
+<!-- Mobile Header -->
+<header class="mobile-header">
+    <div class="header_logo">
+        <img src="assets/images/weldmartlogo1.png" alt="Weldmart Logo">
+    </div>
+    <div class="header_icons">
+        <div class="cart">
+            <div class="cart_icon">
+                <a href="cart.php"><i class="fa-solid fa-cart-shopping fa-lg"></i></a>
+            </div>
+            <div id="cart-counter">0</div>
+        </div>
+        <div class="whatsapp_icon">
+            <a href="http://wa.me/+254711288688" target="_blank" rel="noopener noreferrer">
+                <i class="fa-brands fa-square-whatsapp fa-xl"></i>
+            </a>
+        </div>
+        <button class="hamburger" id="hamburger">
+            <i class="fa-solid fa-bars"></i>
+        </button>
+    </div>
+</header>
+
+<!-- Hidden Nav Menu for Mobile -->
+<div class="mobile-nav" id="mobile-nav">
+    <ul class="nav_links">
+        <li><a href="index.php" class="<?= $currentPage == 'index.php' ? 'active' : '' ?>">Home</a></li>
+        <li><a href="products.php" class="<?= $currentPage == 'products.php' ? 'active' : '' ?>">Products</a></li>
+        <li><a href="about.php" class="<?= $currentPage == 'about.php' ? 'active' : '' ?>">About Us</a></li>
+        <li><a href="contact.php" class="<?= $currentPage == 'contact.php' ? 'active' : '' ?>">Contact</a></li>
+    </ul>
+</div>
