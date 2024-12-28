47ff6807942ed3f5df2ca951e3916439bf9b60d7
Lennox Kabo <borelennox@gmail.com>
2024-12-28 22:34:15 +0300

    checkout with whatsapp button fix

--- cart.php
+++ cart.php
@@ -13,7 +13,7 @@
         <div id="cart-items" class="cart_items"></div>
         <div class="cart_checkout">
             <a id="checkout-link" target="_blank">
-                <button class="checkout_button">Checkout with WhatsApp</button>
+                <button class="checkout_button"><i class="fa-brands fa-whatsapp" style="color: #ffffff;"></i> Checkout with WhatsApp</button>
             </a>
             
             <!-- Clear Cart Button -->
